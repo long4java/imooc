86ca5a649a8c9926130e70b8526fa8b76136b01e
liulong <long4java@126.com>
2019-06-16 23:30:07 +0800

    git提交第一次kotin

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.printf("这是kotin提交的内容");
         System.out.println("Hello World!");
     }
 }
---
+++ src/TestAddKotin.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Stone on 2019/6/16.
+ */
+public class TestAddKotin {
+
+    public static void main(String[] args) {
+        System.out.printf("ssss");
+    }
+}
