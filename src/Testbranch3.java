05b0e8969d0a5213c4118950f3bc4c96f0a28254
liulong <long4java@126.com>
2019-07-02 00:17:50 +0800

    分支3

---
+++ src/Testbranch3.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Stone on 2019/7/2.
+ */
+public class Testbranch3 {
+
+    public static void main(String[] args) {
+        System.out.printf("qqq");
+    }
+
+}
---
+++ src/tersrrr.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Stone on 2019/7/2.
+ */
+public class tersrrr {
+    private int ss;
+}
